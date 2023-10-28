5797458d13e233a81f1afe4f5a90dacab66896b8
mahak <mahakmahmood9@gmail.com>
2023-10-28 14:51:03 -0700

    commiting after update

--- dualaccess.py
+++ dualaccess.py
@@ -1,2 +1,9 @@
 #This is first code when file created
 print("Hello pink")
+#I am updating and commiting after remoteuser have already commited the same file
+value =100
+quantity = 5
+print("Total payable amount is: ", value*quantity)
+
+#Done!
+
