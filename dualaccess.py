d748110e276ec2abafb2f0002163686784450f3e
mahak <mahakmahmood9@gmail.com>
2023-10-29 08:43:14 -0700

    dualaccess file removed

--- dualaccess.py
+++
@@ -1,9 +0,0 @@
-#This is first code when file created
-print("Hello pink")
-#I am updating and commiting after remoteuser have already commited the same file
-value =100
-quantity = 5
-print("Total payable amount is: ", value*quantity)
-
-#Done!
-
